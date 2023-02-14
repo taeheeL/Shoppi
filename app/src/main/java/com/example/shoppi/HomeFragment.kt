7053221c31629e9f2b2a7a5fe7a0bb2e23765402
taeheeL <t9h9@naver.com>
2023-02-15 00:52:00 +0900

    Navigation Component로 Fragment 생성 & 연결

---
+++ app/src/main/java/com/example/shoppi/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.shoppi
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class HomeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
+        button.setOnClickListener {
+           findNavController().navigate(R.id.action_home_to_product_detail)
+        }
+    }
+}
--- app/src/main/java/com/example/shoppi/MainActivity.kt
+++ app/src/main/java/com/example/shoppi/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.shoppi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
+
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
+
     }
 
 }
