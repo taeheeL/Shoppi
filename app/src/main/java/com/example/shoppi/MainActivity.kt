af5f492ddd1b7a238223abd25420a0f2537a9129
taeheeL <t9h9@naver.com>
2023-02-14 22:03:56 +0900

    Splash 구현 (API 31고려)

--- app/src/main/java/com/example/shoppi/MainActivity.kt
+++ app/src/main/java/com/example/shoppi/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.shoppi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
     }
+
 }
