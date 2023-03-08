9d30f63463e1e02b03a356d01c389b33ca8c9094
taeheeL <t9h9@naver.com>
2023-03-09 00:41:38 +0900

    카테고리 ListAdapter 생성 및 연결

---
+++ app/src/main/java/com/example/shoppi/ui/category/CategoryAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.shoppi.ui.category
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppi.databinding.ItemCategoryBinding
+import com.example.shoppi.model.Category
+
+class CategoryAdapter : ListAdapter<Category, CategoryAdapter.CategoryViewHolder>(CategoryDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val binding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class CategoryViewHolder(private val binding: ItemCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(category: Category){
+                binding.category = category
+                binding.executePendingBindings()
+            }
+
+    }
+}
+
+class CategoryDiffCallback: DiffUtil.ItemCallback<Category>(){
+    override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem.categoryId == newItem.categoryId
+    }
+
+    override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem == newItem
+    }
+
+}
--- app/src/main/java/com/example/shoppi/ui/common/ViewModelFactory.kt
+++ app/src/main/java/com/example/shoppi/ui/common/ViewModelFactory.kt
@@ -4,18 +4,29 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppi.AssetLoader
-import com.example.shoppi.repository.HomeAssetDataSource
-import com.example.shoppi.repository.HomeRepository
+import com.example.shoppi.network.ApiClient
+import com.example.shoppi.repository.category.CategoryRemoteDataSource
+import com.example.shoppi.repository.category.CategoryRepository
+import com.example.shoppi.repository.home.HomeAssetDataSource
+import com.example.shoppi.repository.home.HomeRepository
+import com.example.shoppi.ui.category.CategoryViewModel
 import com.example.shoppi.ui.home.HomeViewModel
 
 class ViewModelFactory(private val context: Context): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(HomeViewModel::class.java)){
-            // TODO : DI 사용
-            val repository = HomeRepository(HomeAssetDataSource(AssetLoader(context)))
-            return HomeViewModel(repository) as T
-        } else {
-            throw java.lang.IllegalArgumentException("Failed to create ViewModel: ${modelClass.name}")
+        return when {
+            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
+                // TODO : DI 사용
+                val repository = HomeRepository(HomeAssetDataSource(AssetLoader(context)))
+                HomeViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(CategoryViewModel::class.java) -> {
+                val repository = CategoryRepository(CategoryRemoteDataSource(ApiClient.create()))
+                CategoryViewModel(repository) as T
+            }
+            else -> {
+                throw java.lang.IllegalArgumentException("Failed to create ViewModel: ${modelClass.name}")
+            }
         }
     }
 }
