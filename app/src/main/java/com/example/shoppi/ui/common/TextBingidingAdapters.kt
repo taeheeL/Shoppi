345a676bc32fa2469f77f301ef8ebeed5a177e00
taeheeL <t9h9@naver.com>
2023-03-21 22:31:32 +0900

    Add StrikeThrough BindingAdapter

--- app/src/main/java/com/example/shoppi/ui/common/TextBingidingAdapters.kt
+++ app/src/main/java/com/example/shoppi/ui/common/TextBingidingAdapters.kt
@@ -1,5 +1,6 @@
 package com.example.shoppi.ui.common
 
+import android.graphics.Paint
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.shoppi.R
@@ -9,11 +10,19 @@ import kotlin.math.roundToInt
 @BindingAdapter("priceAmount")
 fun applyPriceFormat(view: TextView, price: Int) {
     val decimalFormat = DecimalFormat("#,###")
-    view.text  = view.context.getString(R.string.unit_discount_currency, decimalFormat.format(price))
+    view.text = view.context.getString(R.string.unit_discount_currency, decimalFormat.format(price))
 }
 
-@BindingAdapter("priceAmount","discountRate")
-fun applyPriceDiscountRate(view:TextView, price: Int, discountRate:Int){
+@BindingAdapter("priceAmount", "discountRate")
+fun applyPriceDiscountRate(view: TextView, price: Int, discountRate: Int) {
     val discountPrice = (((100 - discountRate) / 100.0) * price).roundToInt()
     applyPriceFormat(view, discountPrice)
+}
+
+@BindingAdapter("priceAmount", "strikeThrough")
+fun applyPriceAndStrikeStyle(view: TextView, price: Int, strikeThrough: Boolean) {
+    applyPriceFormat(view, price)
+    if (strikeThrough) {
+        view.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+    }
 }
